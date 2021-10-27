a6151ef578175d6921c3bd2a67bd7761de06726e
salman602 <salmanrahman602@gmail.com>
2021-10-27 20:34:08 +0600

    first commit

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,4 +1,12 @@
-console.log(process.env);
+// console.log(process.env);
+/* const firebaseConfig = {
+    apiKey: "AIzaSyA-1uQRBxzdZeQQstFp_AGin4qrJknONFM",
+    authDomain: "genius-car-mechanic-fb2e7.firebaseapp.com",
+    projectId: "genius-car-mechanic-fb2e7",
+    storageBucket: "genius-car-mechanic-fb2e7.appspot.com",
+    messagingSenderId: "715104517453",
+    appId: "1:715104517453:web:8bc8fce149255afdeb4b04"
+}; */
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
